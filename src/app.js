9d385cc118234c25e0e9b57a0588cc93f3089267
Matiaslopezp <lopezp.matias@gmail.com>
2022-11-02 23:51:18 +0000

    segundo commit

--- src/app.js
+++ src/app.js
@@ -4,8 +4,30 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import { LoaderOptionsPlugin } from "webpack";
 
 window.onload = function() {
   //write your code here
+  let ArrPalo = ["♦", "♥", "♠", "♣"];
+
+  let ArrNum = [
+    "As",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  let LargoArrPalo = ArrPalo.length;
+  let LargoArrNum = ArrNum.length;
+
   console.log("Hello Rigo from the console!");
 };
